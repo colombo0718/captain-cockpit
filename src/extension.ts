e8ccab9f6b956468318ecbf7d16b7024c54e9f67
colombo0718 <colombo0718@gmail.com>
2026-05-16 15:23:54 +0800

    feat: URI handler 註冊、支援 vscode://leaflune.captain-cockpit/ 啟動

--- src/extension.ts
+++ src/extension.ts
@@ -10,7 +10,15 @@ export function activate(context: vscode.ExtensionContext) {
     CockpitPanel.refresh();
   });
 
-  context.subscriptions.push(openCmd, refreshCmd);
+  // URI handler：讓 vscode://leaflune.captain-cockpit/ 觸發開啟
+  // 用途：Windows 工作列釘選的捷徑、點一下就跳開駕駛艙
+  const uriHandler = vscode.window.registerUriHandler({
+    handleUri() {
+      CockpitPanel.createOrShow(context.extensionUri);
+    }
+  });
+
+  context.subscriptions.push(openCmd, refreshCmd, uriHandler);
 }
 
 export function deactivate() {}
